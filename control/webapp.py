a96d2e0c7c914f75c087f9d85523b69e43e8df41
Richard Allitt <rsa33@srcf.net>
2014-11-08 16:15:46 +0000

    Raven auth

--- control/webapp.py
+++ control/webapp.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, Request, render_template, send_from_directory
+from raven.flask_glue import AuthDecorator
+from srcf.database import Member, Session
+
+class R(Request):
+    trusted_hosts = {'localhost'}
+
 app = Flask(__name__)
 app.debug = True
-# import jinja2
+app.request_class = R
+app.secret_key = "aVw9OormkPyuliR2LVoIq5IQJPK9GznZOT54kP65SeK8QeZrfC"
+auth_decorator = AuthDecorator(desc="SRCF control panel")
+app.before_request(auth_decorator.before_request)
+
+sess = Session()	
 
 @app.route('/')
-def hello_world():
-    return render_template("home.jinja2")
+def home():
+	# auth_decorator.principal
+	return render_template("home.jinja2")
 
 @app.route('/static/<path:filename>')
 def static(filename):
