adcb9a07a3656f0d9677391af4128e3237b200d0
Daniel Richman <main@danielrichman.co.uk>
2014-11-08 21:57:11 +0000

    quick refactor of pgdb/mailman lookups

--- control/webapp.py
+++ control/webapp.py
@@ -11,18 +11,29 @@ app.before_request(auth_decorator.before_request)
 
 sess = _sess()
 
+def lookup_pgdbs(prefix):
+    params = {'prefix': prefix, 'prefixfilter': '%s-%%' % prefix}
+    q = sess.execute('SELECT datname FROM pg_database ' \
+                     'JOIN pg_roles ON datdba=pg_roles.oid ' \
+                     'WHERE rolname=:prefix OR rolname LIKE :prefixfilter',
+                      params)
+    return q.fetchall()
+
+
+def lookup_lists(prefix):
+    patterns = "/var/lib/mailman/lists/%s-*" % prefix
+    return [os.path.basename(ldir) for ldir in glob.iglob(patterns)]
+
 @app.route('/')
 def home():
     crsid = auth_decorator.principal
     mem = get_member(crsid)
-    params = {'crsid': crsid, 'crsidfilter': '%s-%%' % crsid}
-    mem.pgdbs = sess.execute('SELECT datname FROM pg_database JOIN pg_roles ON datdba=pg_roles.oid WHERE rolname=:crsid OR rolname LIKE :crsidfilter', params).fetchall()
-    mem.lists = [os.path.basename(ldir) for ldir in glob.iglob("/var/lib/mailman/lists/%s-*" % crsid)]
+    mem.pgdbs = lookup_pgdbs(crsid)
+    mem.lists = lookup_lists(crsid)
     for soc in mem.societies:
-        socid = soc.name
-        params = {'socid': socid, 'socidfilter': '%s-%%' % socid}
-        soc.pgdbs = sess.execute('SELECT datname FROM pg_database JOIN pg_roles ON datdba=pg_roles.oid WHERE rolname=:crsid OR rolname LIKE :crsidfilter', params).fetchall()
-        soc.lists = [os.path.basename(ldir) for ldir in glob.iglob("/var/lib/mailman/lists/%s-*" % socid)]
+        soc.pgdbs = lookup_pgdbs(soc.society)
+        soc.lists = lookup_lists(soc.society)
+
     return render_template("home.html", member=mem)
 
 if __name__ == '__main__':
