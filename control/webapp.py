e1965f9c9121abf5329bd5ad89e100afc4982e63
Daniel Richman <main@danielrichman.co.uk>
2014-11-08 19:30:45 +0000

    Some housekeeping, separate dev and deploy envs

--- control/webapp.py
+++ control/webapp.py
@@ -2,23 +2,10 @@ from flask import Flask, Request, render_template, send_from_directory
 from raven.flask_glue import AuthDecorator
 from srcf.database.queries import get_member, get_society
 
-class R(Request):
-    trusted_hosts = {'localhost', 'srcf-admin.soc.srcf.net'}
-
 app = Flask(__name__)
-app.debug = True
-app.request_class = R
-app.secret_key = "aVw9OormkPyuliR2LVoIq5IQJPK9GznZOT54kP65SeK8QeZrfC"
 auth_decorator = AuthDecorator(desc="SRCF control panel")
 app.before_request(auth_decorator.before_request)
 
 @app.route('/')
 def home():
-    return render_template("home.jinja2", member=get_member(auth_decorator.principal))
-
-@app.route('/static/<path:filename>')
-def static(filename):
-    return send_from_directory('static', filename)
-
-if __name__ == '__main__':
-    app.run()
+    return render_template("home.html", member=get_member(auth_decorator.principal))
--- deploy/apache-magic/flask.cgi
+++ deploy/apache-magic/flask.cgi
@@ -1,11 +1,21 @@
 #!/usr/bin/python
 from wsgiref.handlers import CGIHandler
 from werkzeug.debug import DebuggedApplication
+from flask import Request
 
 import sys
+from os import path
+
 sys.path.insert(0, "/societies/srcf-admin/control-hackday")
 
 from control.webapp import app
+
+class R(Request):
+    trusted_hosts = {'srcf-admin.soc.srcf.net'}
+app.request_class = R 
+
+with open(path.join(path.dirname(__file__), 'secret')) as f:
+    app.secret_key = f.read()
 
 class AppRootFixer(object):
     def __init__(self, app):
