16111ae7128af849e50c665d7c1cc5215bdb21e8
Richard Allitt <rsa33@srcf.net>
2014-11-08 15:39:37 +0000

    Sample page layout

--- control/webapp.py
+++ control/webapp.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+app.debug = True
+# import jinja2
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template("home.jinja2")
 
 if __name__ == '__main__':
     app.run()
