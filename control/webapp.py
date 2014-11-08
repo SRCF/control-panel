a95744d4d4197096d311f65ed88ad79ff17dc129
Richard Allitt <rsa33@srcf.net>
2014-11-08 15:52:57 +0000

    Neaten up code

--- control/webapp.py
+++ control/webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
 app.debug = True
 # import jinja2
@@ -6,6 +6,10 @@ app.debug = True
 @app.route('/')
 def hello_world():
     return render_template("home.jinja2")
+
+@app.route('/static/<path:filename>')
+def static(filename):
+    return send_from_directory('static', filename)
 
 if __name__ == '__main__':
     app.run()
