e106fbc2fb5d27ba57b3bd4a10437a9fb009d696
Daniel Richman <main@danielrichman.co.uk>
2014-12-24 17:25:19 +0000

    show pgdbs, mysqldbs

--- control/webapp.py
+++ control/webapp.py
@@ -3,11 +3,13 @@ import glob
 import re
 import functools
 import operator
+import ConfigParser
 
 import flask
 from flask import Flask, Request, render_template, redirect, url_for, request
 import jinja2
 import sqlalchemy.orm
+import MySQLdb
 from raven.flask_glue import AuthDecorator
 
 import srcf.database.queries
@@ -17,12 +19,12 @@ from . import jobs
 
 
 ## App setup
-
 app = Flask(__name__)
 
 auth_decorator = AuthDecorator(desc="SRCF control panel")
 app.before_request(auth_decorator.before_request)
 
+# Most database is done with postgres; see `srcf.database`
 # This snippet is adapted from Flask-SQLAlchemy
 sess = sqlalchemy.orm.scoped_session(
     srcf.database.Session,
@@ -32,6 +34,24 @@ sess = sqlalchemy.orm.scoped_session(
 def teardown_request(res):
     sess.remove()
     return res
+
+# We occasionally need to interrogate the list of MySQL databases
+my_cnf = ConfigParser.ConfigParser()
+my_cnf.read("/societies/srcf-admin/.my.cnf")
+mysql_passwd = my_cnf.get('client', 'password')
+def mysql_connection():
+    if not hasattr(flask.g, "mysql"):
+        # A throwaway connection
+        flask.g.mysql = \
+            MySQLdb.connect(user="srcf_admin", db="srcf_admin",
+                            passwd=mysql_passwd)
+        flask.g.mysql.autocommit = True
+    return flask.g.mysql
+
+@app.teardown_request
+def teardown_request(res):
+    if hasattr(flask.g, "mysql"):
+        flask.g.mysql.close()
 
 
 ## Template helpers
@@ -48,11 +68,20 @@ app.jinja_env.globals["sif"] = sif
 ## Helpers
 def lookup_pgdbs(prefix):
     params = {'prefix': prefix, 'prefixfilter': '%s-%%' % prefix}
+    # we can borrow the postgres connection we already have
     q = sess.execute('SELECT datname FROM pg_database ' \
                      'JOIN pg_roles ON datdba=pg_roles.oid ' \
                      'WHERE rolname=:prefix OR rolname LIKE :prefixfilter',
                       params)
-    return q.fetchall()
+    return [row[0] for row in q.fetchall()]
+
+def lookup_mysqldbs(prefix):
+    cur = mysql_connection().cursor()
+    perfix = prefix.replace("-", "_")
+    q = "SELECT SCHEMA_NAME FROM information_schema.schemata WHERE " \
+        "SCHEMA_NAME = %s OR SCHEMA_NAME LIKE %s"
+    cur.execute(q, (prefix, prefix + '/%'))
+    return [row[0] for row in cur]
 
 
 def lookup_lists(prefix):
@@ -68,9 +97,11 @@ def home():
     except KeyError:
         return redirect(url_for('signup'))
 
+    mem.mysqldbs = lookup_mysqldbs(crsid)
     mem.pgdbs = lookup_pgdbs(crsid)
     mem.lists = lookup_lists(crsid)
     for soc in mem.societies:
+        soc.mysqldbs = lookup_mysqldbs(soc.society)
         soc.pgdbs = lookup_pgdbs(soc.society)
         soc.lists = lookup_lists(soc.society)
 
