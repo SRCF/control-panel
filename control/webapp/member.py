80ee2df8b1990d99d286f9caa64f142358f0daf6
Richard Allitt <RichardAllitt@live.co.uk>
2015-02-15 16:49:43 +0000

    User password reset form

--- control/webapp/member.py
+++ control/webapp/member.py
@@ -1,7 +1,9 @@
 from werkzeug.exceptions import NotFound
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 
+from .utils import srcf_db_sess as sess
 from . import utils, inspect_services
+from .. import jobs
 
 
 bp = Blueprint("member", __name__)
@@ -17,4 +19,20 @@ def home():
 
     inspect_services.lookup_all(mem)
 
-    return render_template("member.html", member=mem)
+    return render_template("member/home.html", member=mem)
+
+@bp.route("/member/password", methods=["GET", "POST"])
+def reset_password():
+    crsid = utils.raven.principal
+    try:
+        mem = utils.get_member(crsid)
+    except KeyError:
+        raise NotFound
+
+    if request.method == "POST":
+        j = jobs.ResetUserPassword.new(member=mem)
+        sess.add(j.row)
+        sess.commit()
+        return redirect(url_for('job_status.status', id=j.job_id))
+    else:
+        return render_template("member/reset_password.html", member=mem)
