bfbd4cb3a31bbcf48af9dc659093a0f011b76a8d
Richard Allitt <rsa33@srcf.net>
2016-03-20 11:43:43 +0000

    Enforce HTTPS only

--- control/webapp/__init__.py
+++ control/webapp/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, redirect
 
 from . import utils, home, member, society, signup, jobs, admin
 
@@ -12,3 +12,8 @@ app.register_blueprint(society.bp)
 app.register_blueprint(signup.bp)
 app.register_blueprint(jobs.bp)
 app.register_blueprint(admin.bp)
+
+@app.before_request
+def https():
+    if not request.url.startswith("https"):
+        return redirect(request.url.replace("http", "https", 1))
