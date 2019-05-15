6edc0339d203bd5d0efb782f84b033b6af21ab92
Richard Allitt <rsa33@srcf.net>
2019-05-15 19:16:49 +0100

    Add logging config

--- control/webapp/__init__.py
+++ control/webapp/__init__.py
@@ -1,11 +1,18 @@
+import logging
+
 from flask import Flask
 
 from . import utils, home, member, society, signup, jobs, admin
 
+
 app = Flask(__name__,
             template_folder="../templates",
             static_folder="../static")
+
+logging.basicConfig(level=logging.DEBUG if app.debug else logging.INFO)
+
 utils.setup_app(app)
+
 app.register_blueprint(home.bp)
 app.register_blueprint(member.bp)
 app.register_blueprint(society.bp)
